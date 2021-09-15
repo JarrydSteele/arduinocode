cf2dbab361125fdf57bd3a518d0a205cc043cb56
JarrydSteele <jarryd.steele@gmail.com>
2021-09-15 15:40:09 +0200

    add 10dof support

---
+++ VSCode/10dof_imu_sensor/src/main.cpp
@@ -0,0 +1,148 @@
+#include <Arduino.h>
+#include "MPU9250.h"
+#include "EEPROM.h"
+
+MPU9250 mpu;
+
+bool calibrate = false;
+
+float AccBiasX = -38.42, AccBiasY = 11.31, AccBiasZ = 197.08;
+float GyroBiasX = 0.77, GyroBiasY = 1.00, GyroBiasZ = -0.26;
+float MagBiasX = -286.83, MagBiasY = 510.20, MagBiasZ = -287.20;
+float MagScaleX = 1.26, MagScaleY = 0.71, MagScaleZ = 1.26;
+
+float calibrated = 0;
+
+float pitch, roll, yaw;
+
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
+}
+
+void print_calibration() {
+    Serial.println("< calibration parameters >");
+    Serial.println("accel bias [g]: ");
+    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.println();
+    Serial.println("gyro bias [deg/s]: ");
+    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.println();
+    Serial.println("mag bias [mG]: ");
+    Serial.print(mpu.getMagBiasX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasZ());
+    Serial.println();
+    Serial.println("mag scale []: ");
+    Serial.print(mpu.getMagScaleX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleZ());
+    Serial.println();
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  Wire.begin();
+  delay(2000);
+
+  if (!mpu.setup(0x68)) {  // change to your own address
+      while (1) {
+          Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+          delay(5000);
+      }
+  }
+
+  // calibrate anytime you want to
+  if (calibrate)
+  {
+    // calibration sequence
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
+
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
+
+    // get values from calibration
+    AccBiasX = mpu.getAccBiasX();     AccBiasY = mpu.getAccBiasY();     AccBiasZ = mpu.getAccBiasZ();
+    GyroBiasX = mpu.getGyroBiasX();   GyroBiasY = mpu.getGyroBiasY();   GyroBiasZ = mpu.getGyroBiasZ();
+    MagBiasX = mpu.getMagBiasX();     MagBiasY = mpu.getMagBiasY();     MagBiasZ = mpu.getMagBiasZ();
+    MagScaleX = mpu.getMagScaleX();   MagScaleY = mpu.getMagScaleY();   MagScaleZ = mpu.getMagScaleZ();
+    
+    // write calibration values to EEPROM
+    EEPROM.put(0,AccBiasX);   EEPROM.put(4,AccBiasY);     EEPROM.put(8,AccBiasZ);
+    EEPROM.put(12,GyroBiasX);  EEPROM.put(16,GyroBiasY);    EEPROM.put(20,GyroBiasZ);
+    EEPROM.put(24,MagBiasX);   EEPROM.put(28,MagBiasY);     EEPROM.put(32,MagBiasZ);
+    EEPROM.put(36,MagScaleX);  EEPROM.put(40,MagScaleY);   EEPROM.put(44,MagScaleZ);
+    
+    Serial.print("New Calibration Parameters written to EEPROM.");
+    delay(5000);
+
+    print_calibration();
+    mpu.verbose(false);
+    
+    calibrated = 1;
+    EEPROM.put(48,calibrated);
+
+  } 
+  else 
+  {
+    EEPROM.get(48,calibrated);
+    
+    if (calibrated) 
+    {
+      Serial.print("Fetching Calibration Parameters from EEPROM.");
+      EEPROM.get(0,AccBiasX);   EEPROM.get(4, AccBiasY);    EEPROM.get(8, AccBiasZ);
+      EEPROM.get(12,GyroBiasX);  EEPROM.get(16, GyroBiasY);   EEPROM.get(20, GyroBiasZ);
+      EEPROM.get(24,MagBiasX);   EEPROM.get(28, MagBiasY);    EEPROM.get(32, MagBiasZ);
+      EEPROM.get(36,MagScaleX);  EEPROM.get(40, MagScaleY);  EEPROM.get(44, MagScaleZ);
+    }
+    else
+    {
+      Serial.print("Using Default Calibration Parameters.");
+    }
+  
+    mpu.setAccBias(AccBiasX, AccBiasY, AccBiasZ);
+    mpu.setGyroBias(GyroBiasX, GyroBiasY, GyroBiasZ);
+    mpu.setMagBias(MagBiasX, MagBiasY, MagBiasZ);
+    mpu.setMagScale(MagScaleX, MagScaleY, MagScaleZ);
+    delay(5000);
+  }
+}
+
+void loop() {
+    if (mpu.update()) 
+    {
+        yaw = mpu.getYaw();
+        pitch = mpu.getPitch();
+        roll = mpu.getRoll();
+        
+        static uint32_t prev_ms = millis();
+        if (millis() > prev_ms + 25) 
+        {
+            print_roll_pitch_yaw();
+            prev_ms = millis();
+        }
+    }
+}
--- VSCode/sensor_telemetry/src/main.cpp
+++ VSCode/sensor_telemetry/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
-
-
-
-
-
-// Start ADC Init
 #include "Adafruit_ADS1X15.h"
 #include "Wire.h"
+#include "MPU9250.h"
+#include "EEPROM.h"
+#include "SparkFunBME280.h"
+
+const long SERIAL_REFRESH_TIME = 25;
+unsigned long refresh_time;
+
+//FIXME: make work
+
+// Start ADC Init
+
 Adafruit_ADS1X15 ADC1;         //Address (0x48)
 Adafruit_ADS1X15 ADC2;         //Address (0x49)
 
@@ -14,24 +19,68 @@ int16_t adc0, adc1, adc2, adc3, adc4, adc5, adc6, adc7;
 // End ACD Init
 
 // Start IMU Init
-#include "GY521.h"
-GY521 sensor(0x68);
 
-uint32_t counter = 0;
+
+MPU9250 mpu;
+
+bool calibrate = false;
+
+float AccBiasX = -38.42, AccBiasY = 11.31, AccBiasZ = 197.08;
+float GyroBiasX = 0.77, GyroBiasY = 1.00, GyroBiasZ = -0.26;
+float MagBiasX = -286.83, MagBiasY = 510.20, MagBiasZ = -287.20;
+float MagScaleX = 1.26, MagScaleY = 0.71, MagScaleZ = 1.26;
+
+float calibrated = 0;
 
 float pitch, roll, yaw;
+
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
+}
+
+void print_calibration() {
+    Serial.println("< calibration parameters >");
+    Serial.println("accel bias [g]: ");
+    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.println();
+    Serial.println("gyro bias [deg/s]: ");
+    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.println();
+    Serial.println("mag bias [mG]: ");
+    Serial.print(mpu.getMagBiasX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasZ());
+    Serial.println();
+    Serial.println("mag scale []: ");
+    Serial.print(mpu.getMagScaleX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleZ());
+    Serial.println();
+}
 // End IMU Init
 
 // Start Pressure Init
-#include "SparkFunBME280.h"
+
 BME280 PressureSensor; //Uses I2C address 0x76 (jumper closed)
 float pressure, temp_c, altitude;
 // End Pressure Init
-
-const long SERIAL_REFRESH_TIME = 100;
-unsigned long refresh_time;
-
-
 
 void setup(void)
 {
@@ -47,27 +96,79 @@ void setup(void)
 
 // Start IMU Setup
   Wire.begin();
-  Wire.setClock(400000); //Increase to fast I2C speed!  //**************************************
+  //Wire.setClock(400000); //Increase to fast I2C speed!  //**************************************
+  delay(2000);
 
-  delay(100);
-  while (sensor.wakeup() == false)
+  if (!mpu.setup(0x68)) {  // change to your own address
+      while (1) {
+          Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+          delay(5000);
+      }
+  }
+
+  // calibrate anytime you want to
+  if (calibrate)
   {
-    Serial.print(millis());
-    Serial.println("\tCould not connect to GY521");
+    // calibration sequence
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
+
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
+
+    // get values from calibration
+    AccBiasX = mpu.getAccBiasX();     AccBiasY = mpu.getAccBiasY();     AccBiasZ = mpu.getAccBiasZ();
+    GyroBiasX = mpu.getGyroBiasX();   GyroBiasY = mpu.getGyroBiasY();   GyroBiasZ = mpu.getGyroBiasZ();
+    MagBiasX = mpu.getMagBiasX();     MagBiasY = mpu.getMagBiasY();     MagBiasZ = mpu.getMagBiasZ();
+    MagScaleX = mpu.getMagScaleX();   MagScaleY = mpu.getMagScaleY();   MagScaleZ = mpu.getMagScaleZ();
+    
+    // write calibration values to EEPROM
+    EEPROM.put(0,AccBiasX);   EEPROM.put(4,AccBiasY);     EEPROM.put(8,AccBiasZ);
+    EEPROM.put(12,GyroBiasX);  EEPROM.put(16,GyroBiasY);    EEPROM.put(20,GyroBiasZ);
+    EEPROM.put(24,MagBiasX);   EEPROM.put(28,MagBiasY);     EEPROM.put(32,MagBiasZ);
+    EEPROM.put(36,MagScaleX);  EEPROM.put(40,MagScaleY);   EEPROM.put(44,MagScaleZ);
+    
+    Serial.print("New Calibration Parameters written to EEPROM.");
+    delay(1000);
+
+    print_calibration();
+    mpu.verbose(false);
+    
+    calibrated = 1;
+    EEPROM.put(48,calibrated);
+
+  } 
+  else 
+  {
+    EEPROM.get(48,calibrated);
+    
+    if (calibrated) 
+    {
+      Serial.print("Fetching Calibration Parameters from EEPROM.");
+      EEPROM.get(0,AccBiasX);   EEPROM.get(4, AccBiasY);    EEPROM.get(8, AccBiasZ);
+      EEPROM.get(12,GyroBiasX);  EEPROM.get(16, GyroBiasY);   EEPROM.get(20, GyroBiasZ);
+      EEPROM.get(24,MagBiasX);   EEPROM.get(28, MagBiasY);    EEPROM.get(32, MagBiasZ);
+      EEPROM.get(36,MagScaleX);  EEPROM.get(40, MagScaleY);  EEPROM.get(44, MagScaleZ);
+    }
+    else
+    {
+      Serial.print("Using Default Calibration Parameters.");
+    }
+  
+    mpu.setAccBias(AccBiasX, AccBiasY, AccBiasZ);
+    mpu.setGyroBias(GyroBiasX, GyroBiasY, GyroBiasZ);
+    mpu.setMagBias(MagBiasX, MagBiasY, MagBiasZ);
+    mpu.setMagScale(MagScaleX, MagScaleY, MagScaleZ);
     delay(1000);
   }
-  sensor.setAccelSensitivity(2);  // 8g
-  sensor.setGyroSensitivity(1);   // 500 degrees/s
 
-  sensor.setThrottle();
-  
-  // set callibration values from calibration sketch.
-  sensor.axe = 0;
-  sensor.aye = 0;
-  sensor.aze = 0;
-  sensor.gxe = 0;
-  sensor.gye = 0;
-  sensor.gze = 0;
+
+    
 // End IMU Setup
 
 // Start Pressure Setup
@@ -157,22 +258,24 @@ void loop(void)
   // End ADC Readings
 
   // Start IMU Readings
-  sensor.read();
-  pitch = sensor.getPitch();
-  roll  = sensor.getRoll();
-  yaw   = sensor.getYaw();
-  counter++;
+  mpu.update();
+  yaw = mpu.getYaw();
+  pitch = mpu.getPitch();
+  roll = mpu.getRoll();
+  print_roll_pitch_yaw();
+  
+
   // End IMU Readings
 
   // Start Pressure Readings
   pressure = PressureSensor.readFloatPressure();          //Pressure
   temp_c = PressureSensor.readTempC();                    //Temp (C)
-  altitude = PressureSensor.readFloatAltitudeMeters();  //Locally Adjusted Altitude (m)
+  altitude = PressureSensor.readFloatAltitudeMeters();    //Locally Adjusted Altitude (m)
   // End Pressure Readings
 
   if (millis() > refresh_time)
   {
-    printrawtelem();
+    printtelem();
     refresh_time=millis()+SERIAL_REFRESH_TIME;
   }
   
